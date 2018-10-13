ccf58675c2f7361dd9acb37dc032cc6137e9bc32
TA THANH <ta.thanh.03@gmail.com>
2018-10-13 10:19:54 +0200

    Initial Commit

---
+++ Quizz_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizz_Swift
+//
+//  Created by TA Trung Thanh on 13/10/2018.
+//  Copyright © 2018 TA Trung Thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
